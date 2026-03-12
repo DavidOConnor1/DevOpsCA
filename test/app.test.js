a551fdb5b8693e57bda8fb3904a9ef40fa6c869e
DavidOConnor1 <146002230+DavidOConnor1@users.noreply.github.com>
2026-03-12 16:39:40 +0000

    Currently working on the workflow to achieve passes all around

--- test/app.test.js
+++ test/app.test.js
@@ -1,11 +1,50 @@
+//const request = require('supertest');
 const assert = require('assert');
+const app = require('../app');
 
-describe('App', function() {
-  it('should pass a basic test', function() {
-    assert.strictEqual(1, 1);
+describe('Express App', function() {
+  
+  describe('Basic Setup', function() {
+    it('should export the app object', function() {
+      assert.ok(app);
+      assert.strictEqual(typeof app, 'function');
+    });
+
+    it('should have view engine set to ejs', function() {
+      assert.strictEqual(app.get('view engine'), 'ejs');
+    });
+
+    it('should have views directory configured', function() {
+      const views = app.get('views');
+      assert.ok(views.includes('views'));
+    });
   });
 
-  it('should handle math', function() {
-    assert.strictEqual(2 + 2, 4);
+  describe('Middleware', function() {
+    it('should have JSON parser middleware', function() {
+      const middleware = app._router.stack;
+      const hasJsonParser = middleware.some(layer => 
+        layer.name === 'jsonParser' || (layer.handle && layer.handle.name === 'jsonParser')
+      );
+      assert.ok(hasJsonParser);
+    });
+
+    it('should have URL-encoded parser middleware', function() {
+      const middleware = app._router.stack;
+      const hasUrlencodedParser = middleware.some(layer => 
+        layer.name === 'urlencodedParser'
+      );
+      assert.ok(hasUrlencodedParser);
+    });
+
+    it('should have cookie parser middleware', function() {
+      const middleware = app._router.stack;
+      const hasCookieParser = middleware.some(layer => 
+        layer.name === 'cookieParser'
+      );
+      assert.ok(hasCookieParser);
+    });
+
+  
   });
 });
